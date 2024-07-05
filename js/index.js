11d21829b755a58c9f3ed141bc7aa26dce9e140b
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-07-05 20:34:35 +0300

    startJs

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 //Виводить дані на вкладочку "Консоль" у панелі розробника
 
@@ -10,23 +10,76 @@
 // b - назва змінної
 //  = - оператор присвоєння
 // 10 - дані які ми бажаємо присвоїти у комірку(зміну b)
-b = 10;
-x = b + 2;
-console.log(x);
+// b = 10;
+// x = b + 2;
+
+// console - Звернення до консолі.
+// .log() - Покажи результат в консоль
+// x - будь-що що ви бажаєте побачити в консолі
+// console.log(x);
 
 // 0101010101010010101001000101001010101010101001010101
-name = "Artem";
-console.log(name);
+// name = "Artem";
+// console.log(name);
 // ВЛАСТИВІСТЬ - якась описова характеристика
 // name.length - через крапку ми з вами звертаємось до властивостей елементу
 // name - імʼя .length - довжина імені в символах
-nameLength = name.length;
-console.log(nameLength);
+// nameLength = name.length;
+// console.log(nameLength);
 
 // МЕТОД - дозволяє зробити дію
 
 // методи ми також пишемо через крапку перед назвою методу та з дужками крушлими у кінці
 
-console.log(name.toUpperCase());
+// console.log(name.toUpperCase());
 
 //
+
+// const
+// let
+// Hoisting - всплиття
+
+// var name = "Artem";
+//const - незмінне значееня
+const name = "Artem";
+
+let age = 23;
+console.log(age);
+age = age + 1;
+console.log(age);
+
+console.log(name);
+// ТИПИ ДАНИХ
+
+//  "String" - строка
+const string = "String";
+// оператор typeof дозволяє визначити тип даних у змінній.
+console.log(typeof string);
+const number = 10;
+console.log(typeof number);
+const bigInt = 9007199254740991n;
+console.log(typeof bigInt);
+const trueBoolean = true;
+const falseBoolean = false;
+console.log(typeof trueBoolean);
+
+const symbol = Symbol("qwe");
+console.log(typeof symbol);
+
+const ul = null; //null - ми створюємо руками
+
+const undefinedd = undefined; // undefined - нічого не знайдено
+
+alert("ПОВІДОМЛЕННЯ"); // блокує виконання коду та виводить на екран у вигляді вспливаючого вікна повідомлення
+
+const asd = prompt("Як у вас справи?"); // Спосіб отримати відповідь письмову від користувача
+
+console.log(asd);
+
+const isOk = confirm("Чи підтверджуєте ви перехід на іншу сторінку?");
+
+if (isOk === true) {
+  window.location.href = "https://www.google.com/";
+} else {
+  console.log("Ви відмовились");
+}
