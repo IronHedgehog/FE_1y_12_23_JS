294304530b5854348106260d0e917a0b16cd441e
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-08-07 21:01:19 +0300

    events dz

--- dz.js
+++ dz.js
@@ -1,5 +1,24 @@
 // 1.Написати скрипт відкриття та закриття модального вікна по кнопці.
 
+//Щоб отримати доступ до будь-якого елементу HTML у js
+// ми маєм застосувати getElementById("id") ,querySelector(".button")
+
+// document - це весь наш html документ
+// .querySelector() - пошуковий метод
+const openButton = document.querySelector("#btn1");
+
+// Як додати подію на елемент
+openButton.addEventListener("click", onClickOpenModal);
+
+// Створюємо обробник події
+
+function onClickOpenModal(e) {
+  //Дозволяє додати на body клас show modal
+  document.body.classList.add("show-modal");
+}
+
+document.body.classList.remove("show-modal");
+
 // 2.Використовуючи попередній код написати скрипт закриття модального вікна по кліку на бекдроп
 
 // 3.Напиши скрипт в якому є три радіобатони зі значенями кольрів. При обиранніна будь якого колір фону боді змінюється відповідно
