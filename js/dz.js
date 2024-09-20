2a7c45e88d95d0308c4ee4da754d1ad63d5c0b94
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-09-20 19:46:05 +0300

    a

--- js/dz.js
+++ js/dz.js
@@ -17,3 +17,65 @@ const logItems = function (array) {
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+//
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+// проходити перевірку на довжину від 4 до 16-ти символів включно
+
+// властивість яка повертає довжину рядка - length
+// Створити розгалудження (if else)
+// Логічний оператор І
+// умова для нашого розгалудження довжину від 4 до 16-ти символів включно
+// Повернути в зовнішній код якщо умова справджується true якщо ні то false
+const isLoginValid = function (login) {
+  // if (login.length >= 4 && login.length <= 16) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  // && - повертає true або false
+  return login.length >= 4 && login.length <= 16;
+};
+// бути унікальним, тобто бути відсутнім в масиві logins
+
+//allLogins - масив всіх логінів які вже є
+//login - логін який ввів користувач який хоче зареєструватись
+//якщо  login  присутній у масиві allLogins ми маємо повернути false
+//якщо  login  відсутній у масиві allLogins ми маємо повернути true
+
+const isLoginUnique = function (allLogins, login) {
+  // includes - повертає true або false залежно від того чи є елемент у масиві
+  // if (allLogins.includes(login)) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  // ! - логічний оператор НЕ (який обертає все навпаки)
+  return !allLogins.includes(login);
+};
+// якщо всі перевірки проїдені має ваш логін додати у масив користувачів
+const addLogin = function (allLogins, login) {
+  // Так виглядає виклик функції isLoginValid();
+  // Метод від оберененого (якщо наша функція повертає результат який нас не влаштовує ми завершуємо виконання функції (Guard close))
+  if (!isLoginValid(login)) {
+    // return - завершує виконання функції
+    return "Логін має бути від 4 до 16 буков";
+  }
+
+  if (!isLoginUnique(allLogins, login)) {
+    return "Такий логін вже є";
+  }
+
+  allLogins.push(login);
+  return "Ти гідний реєстрації";
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(addLogin(logins, "Ajax")); // 'Логін успішно доданий!'
+console.log(addLogin(logins, "robotGoogles")); // 'Такий логін вже використовується!'
+console.log(addLogin(logins, "Zod")); // 'Помилка! Логін повинен бути від 4 до 16 символів'
+console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Помилка! Логін повинен бути від 4 до 16 символів'
