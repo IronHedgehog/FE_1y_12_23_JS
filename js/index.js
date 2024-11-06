19c8d7750f4cb003ca3133b47881575346a9a596
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-11-06 21:36:08 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,146 +1,72 @@
-const userss = [
-  {
-    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
+const salary = 30000;
+const bonus = 10000;
+const rate = 100;
+const overtime = 30;
+
+function getWage(salary, bonus, rate, overtime) {
+  return salary + bonus + rate * overtime;
+}
+
+getWage(salary, bonus, rate, overtime);
+
+const artemFaust = {
+  salary: 30000,
+  bonus: 10000,
+  rate: 100,
+  overtime: 30,
+
+  getWage() {
+    return this.salary + this.bonus + this.rate * this.overtime;
   },
-  {
-    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    id: "249b6175-5c30-44c6-b154-f120923736f5",
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    id: "e1bf46ab-7168-491e-925e-f01e21394812",
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
+};
+
+console.log(artemFaust.getWage());
+
+class Employee {
+  constructor(name, salary, bonus, rate, overtime) {
+    this.name = name;
+    this.salary = salary;
+    this.bonus = bonus;
+    this.rate = rate;
+    this.overtime = overtime;
+  }
+
+  getWage() {
+    return this.salary + this.bonus + this.rate * this.overtime;
+  }
+}
+
+const petro = new Employee("Petro", 30000, 10000, 100, 30);
+
+const employees = [
+  { name: "qwe", salary: 30000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 40000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 50000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 60000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 70000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 80000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 90000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 100000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 11000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 3110000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 30000, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 300100, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 300010, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 300001, bonus: 10000, rate: 100, overtime: 30 },
+  { name: "qwe", salary: 300001, bonus: 10000, rate: 100, overtime: 30 },
 ];
 
-// let totalbalanc = 0;
-// userss.forEach((user) => {
-//   totalbalanc += user.balance;
-// });
-// console.log(totalbalanc);
-
-const totalBalance = userss.reduce((acc, user) => (acc += user.balance), 0);
-
-console.log(totalBalance);
-
-const maleg = userss.filter((user) => {
-  return user.gender === "male";
-});
-console.log(maleg);
-
-const males = userss.reduce((acc, user) => {
-  if (user.gender === "male") {
-    acc.push(user);
-  }
-  return acc;
-}, []);
-
-const arrNumbers = [2, 10, 3, 15, 1];
-
-const alphabet = ["a", "c", "B"];
-
-console.log(
-  alphabet.sort((a, b) => {
-    return b.localeCompare(a);
-  })
+const work = employees.map(
+  (employee) =>
+    new Employee(
+      employee.name,
+      employee.salary,
+      employee.bonus,
+      employee.rate,
+      employee.overtime
+    )
 );
 
-// console.log(
-//   alphabet.sort((a, b) => {
-//     return b - a;
-//   })
-// );
+const elem1 = work[12];
 
-console.log(userss.map((user) => user).sort((a, b) => b.age - a.age));
-// Як можна скопіювати масив, щоб не змінювати оригінальний?
-
-const array15 = [-1, 22, 11, 44, 15];
-
-const array16 = [...array15]; // 1,2,3,4,5
-
-const array17 = array15.slice();
-
-const [first, second, ...others] = array15;
-
-console.log(array15);
-console.log(array16);
-console.log(array17);
-console.log(first);
-console.log(second);
-console.log(others);
-// SORT - мотує початковий масив
-
-console.log(array15.slice().sort());
-console.log(array15);
+console.log(elem1.getWage());
