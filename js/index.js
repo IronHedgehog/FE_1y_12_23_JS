05ba652dda6601526db84c0b4163a2ef09caec56
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-11-08 20:32:14 +0200

    a

--- js/index.js
+++ js/index.js
@@ -23,21 +23,29 @@ const artemFaust = {
 console.log(artemFaust.getWage());
 
 class Employee {
+  #hardwork;
+
   constructor(name, salary, bonus, rate, overtime) {
     this.name = name;
     this.salary = salary;
     this.bonus = bonus;
     this.rate = rate;
     this.overtime = overtime;
+    this.#hardwork = true;
   }
-
+  // Приватні властивості треба використовувати тільки всередині класу
+  #employeeMethod() {
+    return 1 + 2;
+  }
   getWage() {
+    this.#employeeMethod();
     return this.salary + this.bonus + this.rate * this.overtime;
   }
 }
 
 const petro = new Employee("Petro", 30000, 10000, 100, 30);
 
+// petro.#employeeMethod;
 const employees = [
   { name: "qwe", salary: 30000, bonus: 10000, rate: 100, overtime: 30 },
   { name: "qwe", salary: 40000, bonus: 10000, rate: 100, overtime: 30 },
@@ -70,3 +78,39 @@ const work = employees.map(
 const elem1 = work[12];
 
 console.log(elem1.getWage());
+
+class SuperEmployee extends Employee {
+  constructor(name, salary, bonus, rate, overtime, hardWork) {
+    super(name, salary, bonus, rate, overtime);
+    this.hardWork = hardWork;
+  }
+}
+
+const Petro = new SuperEmployee("Petro", 30000, 10000, 100, 30, true);
+
+console.log(Petro.getWage());
+
+// const abc = "ABC".;
+
+// console.log();
+
+class Counter {
+  #time;
+
+  constructor(time) {
+    this.#time = time;
+  }
+
+  set time(newTime) {
+    return (this.#time = newTime);
+  }
+
+  get time() {
+    return this.#time;
+  }
+}
+
+const timer = new Counter(10000);
+timer.time = 20000;
+
+console.log(timer.time);
