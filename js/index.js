3fd432ed5e80055d0c298ccc629ce16059bf4877
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-11-13 21:32:02 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,116 +1,35 @@
-const salary = 30000;
-const bonus = 10000;
-const rate = 100;
-const overtime = 30;
+// DOM
+// class="input"
+const list = document.querySelector("#ul");
+const input = document.querySelector(".input");
+// const list = document.querySelectorAll;
+// const list = document.getElementById;
 
-function getWage(salary, bonus, rate, overtime) {
-  return salary + bonus + rate * overtime;
-}
+list.style.backgroundColor = "red";
 
-getWage(salary, bonus, rate, overtime);
+list.classList.add("list");
+list.classList.remove("list");
+console.log(list.classList.contains("list"));
+input.classList.replace("input", "wrong-input");
 
-const artemFaust = {
-  salary: 30000,
-  bonus: 10000,
-  rate: 100,
-  overtime: 30,
+const img = document.createElement("img");
+// const img1 = document.createElement("img");
 
-  getWage() {
-    return this.salary + this.bonus + this.rate * this.overtime;
-  },
-};
+img.src =
+  "https://gptchat.in.ua/wp-content/uploads/2023/02/iryna_kunichenko_many_colored_balloons_over_the_grand_canyon_hd_06263781-130a-4137-a4c8-2e7454b401c7-1024x585.jpg";
 
-console.log(artemFaust.getWage());
+img.alt = "Тут має буть зображення";
+// img1.src =
+//   "https://gptchat.in.ua/wp-content/uploads/2023/02/iryna_kunichenko_many_colored_balloons_over_the_grand_canyon_hd_06263781-130a-4137-a4c8-2e7454b401c7-1024x585.jpg";
 
-class Employee {
-  #hardwork;
+// img1.alt = "Тут має буть зображення";
 
-  constructor(name, salary, bonus, rate, overtime) {
-    this.name = name;
-    this.salary = salary;
-    this.bonus = bonus;
-    this.rate = rate;
-    this.overtime = overtime;
-    this.#hardwork = true;
-  }
-  // Приватні властивості треба використовувати тільки всередині класу
-  #employeeMethod() {
-    return 1 + 2;
-  }
-  getWage() {
-    this.#employeeMethod();
-    return this.salary + this.bonus + this.rate * this.overtime;
-  }
-}
+console.log(img);
 
-const petro = new Employee("Petro", 30000, 10000, 100, 30);
+// document.body.append(img);
+// document.body.prepend(img);
+img.width = "479";
+// img1.width = "479";
 
-// petro.#employeeMethod;
-const employees = [
-  { name: "qwe", salary: 30000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 40000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 50000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 60000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 70000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 80000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 90000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 100000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 11000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 3110000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 30000, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 300100, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 300010, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 300001, bonus: 10000, rate: 100, overtime: 30 },
-  { name: "qwe", salary: 300001, bonus: 10000, rate: 100, overtime: 30 },
-];
-
-const work = employees.map(
-  (employee) =>
-    new Employee(
-      employee.name,
-      employee.salary,
-      employee.bonus,
-      employee.rate,
-      employee.overtime
-    )
-);
-
-const elem1 = work[12];
-
-console.log(elem1.getWage());
-
-class SuperEmployee extends Employee {
-  constructor(name, salary, bonus, rate, overtime, hardWork) {
-    super(name, salary, bonus, rate, overtime);
-    this.hardWork = hardWork;
-  }
-}
-
-const Petro = new SuperEmployee("Petro", 30000, 10000, 100, 30, true);
-
-console.log(Petro.getWage());
-
-// const abc = "ABC".;
-
-// console.log();
-
-class Counter {
-  #time;
-
-  constructor(time) {
-    this.#time = time;
-  }
-
-  set time(newTime) {
-    return (this.#time = newTime);
-  }
-
-  get time() {
-    return this.#time;
-  }
-}
-
-const timer = new Counter(10000);
-timer.time = 20000;
-
-console.log(timer.time);
+// document.body.after(img);
+document.body.before(img);
