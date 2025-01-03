d04b4b261e5255b975d42eea6008cc9f37d264d0
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-01-03 20:26:57 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,12 +1,48 @@
-const rock = document.querySelector("#rock");
+const button = document.querySelector(".button");
+const buttonAll = document.querySelectorAll(".button");
+const buttonList = document.querySelector(".button-list");
+const buttonItemList = document.querySelectorAll(".button-item");
 
-const choices = ["ножиці", "каміння", "папір"];
-let computerScore = 0;
-let userScore = 0;
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+const third = document.querySelector(".third");
 
-rock.addEventListener("click", onClick);
+parent.addEventListener("click", (e) => {
+  console.log("Parent click");
+});
+child.addEventListener("click", (e) => {
+  console.log("Child click");
+});
+third.addEventListener("click", (e) => {
+  console.log("Third click");
+});
 
-function onClick(eventObject) {
-  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  console.log(computerChoice);
-}
+// console.log(buttonList);
+
+buttonList.addEventListener("click", (e) => {
+  // e.currentTarget - це елемент на якому висить обробник події(addEventListener)
+  // e.target - це елемент на якому відбулася подія
+  // nodeName - дозволяє отримати назву тегу
+  // e.target.nodeName; - дозволяє отримати назву тегу на якому відбулась подія
+  // якщо назва тегу на якому відбулась подія не BUTTON то ми зупиняємо виконання функції(Функція не виконуєься взагалі)
+  console.log(e.currentTarget);
+  if (e.target.nodeName !== "BUTTON") return;
+  console.log(e.target.textContent);
+  // console.log("Відпрацювання юлки");
+});
+
+// buttonAll.forEach((button) =>
+//   button.addEventListener("click", (e) => {
+//     console.log("Ти клацнув на кнопку!");
+//   })
+// );
+
+// buttonItemList.forEach((button) =>
+//   button.addEventListener("click", (e) => {
+//     console.log("Ти клацнув на лішку!");
+//   })
+// );
+
+// button.addEventListener("click", (e) => {
+//   console.log("Ти клацнув на кнопку!");
+// });
