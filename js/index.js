74b80fde4f63d99400cb9d40526a7afed533f5e9
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-01-10 20:23:54 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,91 +1,38 @@
-const button = document.querySelector(".button");
-const buttonAll = document.querySelectorAll(".button");
-const buttonList = document.querySelector(".button-list");
-const buttonItemList = document.querySelectorAll(".button-item");
+// console.log(window);
 
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-const third = document.querySelector(".third");
+// console.log(_ = lodash);
+const addNumbers = _.add(6, 4);
 
-parent.addEventListener("click", (e) => {
-  console.log("Parent click");
-});
-child.addEventListener("click", (e) => {
-  e.stopPropagation(); // блокує вспливання події
-  console.log("Child click");
-});
-third.addEventListener("click", (e) => {
-  e.stopImmediatePropagation(); // Блокує обробники події на цьому елемнті
-  console.log("Third click");
-});
+console.log(addNumbers);
 
-// console.log(buttonList);
+// document.addEventListener("scroll", _.throttle(scrollFunction, 250));
 
-buttonList.addEventListener("click", (e) => {
-  // e.currentTarget - це елемент на якому висить обробник події(addEventListener)
-  // e.target - це елемент на якому відбулася подія
-  // nodeName - дозволяє отримати назву тегу
-  // e.target.nodeName; - дозволяє отримати назву тегу на якому відбулась подія
-  // якщо назва тегу на якому відбулась подія не BUTTON то ми зупиняємо виконання функції(Функція не виконуєься взагалі)
-  console.log(e.currentTarget);
-  if (e.target.nodeName !== "BUTTON") return;
-  console.log(e.target.textContent);
-  // console.log("Відпрацювання юлки");
-});
+// let counter = 0;
 
-// buttonAll.forEach((button) =>
-//   button.addEventListener("click", (e) => {
-//     console.log("Ти клацнув на кнопку!");
-//   })
-// );
+// function scrollFunction(e) {
+//   console.log("1");
+//   counter += 1;
+//   document.body.textContent = counter;
+// }
 
-// buttonItemList.forEach((button) =>
-//   button.addEventListener("click", (e) => {
-//     console.log("Ти клацнув на лішку!");
-//   })
-// );
+const input = document.querySelector("#input");
+// input - Дозволяє отримувати текст який друкує користувач
+// debounce - запускає обробник подій лише один раз пілся того як користувач завершив друк тексту
+input.addEventListener("input", _.debounce(onInput, 300));
 
-// button.addEventListener("click", (e) => {
-//   console.log("Ти клацнув на кнопку!");
-// });
+let counterInput = 0;
 
-const colorPalette = document.querySelector(".color-palette");
-const selectedColor = document.querySelector(".selected-color");
-
-colorPalette.addEventListener("click", onClickPalletButton);
-
-function onClickPalletButton(e) {
-  if (e.target.nodeName !== "BUTTON") return;
-  console.log(e.target.textContent);
-  selectedColor.textContent = e.target.dataset.color;
-  selectedColor.style.backgroundColor = e.target.dataset.color;
+function onInput(e) {
+  console.log(e.target.value);
+  counterInput += 1;
+  console.log(counterInput);
 }
+document.addEventListener("scroll", _.debounce(scrollFunction, 250));
 
-function createColorPalette() {
-  const buttons = [];
-  for (let i = 0; i < 100; i++) {
-    const color = getRandomColor();
-    const button = document.createElement("button");
-    // button.textContent = color;
-    button.dataset.color = color;
-    button.style.backgroundColor = color;
-    button.classList.add("item");
-    button.type = "button";
-    buttons.push(button);
-  }
-  colorPalette.append(...buttons);
-}
+let counter = 0;
 
-createColorPalette();
-
-console.log(colorPalette);
-
-function getRandomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-}
-
-function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, "0");
+function scrollFunction(e) {
+  console.log("1");
+  counter += 1;
+  document.body.textContent = counter;
 }
