72e5f56f2f03bf01e13fb356d2a5aea2e314c0c1
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-01-31 20:29:59 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,57 +1,11 @@
-// 1) Ми хочемо відпрацювати INTersection observer під час завантаження сторінка
+// import { nanoid } from "nanoid";
+import { nanoid } from "https://cdn.jsdelivr.net/npm/nanoid/nanoid.js";
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  const cards = Array.from(document.querySelectorAll(".card"));
-  const container = document.querySelector(".container");
-  // 2)Ми перевіряєм чи є у користувача доступ до INTersection observer
-  // if (!"IntersectionObserver" in window) return;
-  if ("IntersectionObserver" in window) {
-    //   3)Створити екземпляр класу  IntersectionObserver
-    const observer = new IntersectionObserver((entryArr) => {
-      entryArr.forEach((entry) => {
-        // 5) Зрозуміти,що користувач бачить карточку
-        //   entry.isIntersecting === true - користувач має бачити картку
-        if (entry.isIntersecting === true) {
-          // 6) Логіка(що ми хочемо робити коли бачимо карточки)
-          // entry.target - елемент який ми бачимо
+const model = {};
 
-          const card = entry.target;
-          if (!card.classList.contains("show")) {
-            card.classList.add("show");
-            observer.unobserve(card);
-          }
-        }
-      });
-    });
+model.id = nanoid(10); //=> "V1StGXR8_Z5jdHi6B-myT"
 
-    //   7)Для нескінченного скролу нам необхідно слідкувати за останнім елементом
-    const lastElementObserver = new IntersectionObserver((entryArr) => {
-      // 9)Генерити нові карточки
-      const lastElement = entryArr[0];
-      if (!lastElement.isIntersecting) return;
-      newCards(55);
-      lastElementObserver.unobserve(lastElement.target);
-      lastElementObserver.observe(document.querySelector(".card:last-child"));
-    });
+console.log(model);
 
-    //   4)Ми маємо повідомити обсерверу, що ми хочемо відслідковувати
-    // 8) Даємо нашому observery інформацію що він має відслідковувати(останній елемент)
-
-    lastElementObserver.observe(document.querySelector(".card:last-child"));
-
-    function newCards(quantity) {
-      for (let i = 0; i < quantity; i++) {
-        const card = document.createElement("div");
-        card.classList.add("card");
-        observer.observe(card);
-        container.append(card);
-      }
-    }
-
-    cards.forEach((card) => {
-      observer.observe(card);
-    });
-  } else {
-    console.log("Оновіть браузер");
-  }
-});
+// model.customId = customAlphabet("qйцкнгнщгшлопррваівафячммть1234567890-_?@");
+// console.log(model);
