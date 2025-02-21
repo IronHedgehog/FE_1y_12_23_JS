28c72de8232f9668a0da01f8b23c60c8aa59d063
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-02-21 20:32:56 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,24 +1,48 @@
+// import { comments } from './js/coments';
 import Handlebars from 'handlebars';
-import { comments } from './js/coments';
+import { data } from './js/data';
+import { heroes } from './js/heroes';
 
-const template = document.querySelector('#template').innerHTML.trim();
-const newTemplate = document.getElementById('temp').innerHTML.trim();
+// const template = document.querySelector('#template').innerHTML.trim();
+// const newTemplate = document.getElementById('temp').innerHTML.trim();
 
-const object = {
-  title: 'qweqwe',
-  text: 'asdasd',
-};
+// const object = {
+//   title: 'qweqwe',
+//   text: 'asdasd',
+// };
+
+// console.log(template);
+
+// const shablon = Handlebars.compile(template);
+// console.log(shablon(object));
+
+// document.body.insertAdjacentHTML('beforeend', shablon(object));
+
+// const newShablon = Handlebars.compile(newTemplate);
+
+// document.body.insertAdjacentHTML(
+//   'beforeend',
+//   newShablon({ comments: comments })
+// );
+
+const template = document.getElementById('data').innerHTML.trim();
 
 console.log(template);
 
-const shablon = Handlebars.compile(template);
-console.log(shablon(object));
+const dataTemplate = Handlebars.compile(template);
 
-document.body.insertAdjacentHTML('beforeend', shablon(object));
+console.log(dataTemplate);
+console.log(data);
+console.log(dataTemplate({ data: data }));
 
-const newShablon = Handlebars.compile(newTemplate);
+// document.body.insertAdjacentHTML('beforeend', dataTemplate({ data: data }));
 
-document.body.insertAdjacentHTML(
-  'beforeend',
-  newShablon({ comments: comments })
-);
+const heroesTempalte = document.getElementById('heroes').innerHTML.trim();
+
+console.log(heroesTempalte);
+
+const heroesShablon = Handlebars.compile(heroesTempalte);
+console.log(heroes);
+console.log(heroesShablon({ heroes }));
+
+document.body.insertAdjacentHTML('beforeend', heroesShablon({ heroes }));
