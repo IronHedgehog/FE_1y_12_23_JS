9b9687871414bc724e189ccc0347a3aa9c840472
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-03-12 21:26:34 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,92 +1,134 @@
-// setTimeout - приймає затримку у мілісекундах
-// setTimeout - встановлює перерву перед виконанням коду у середині таймаут
+// // setTimeout - приймає затримку у мілісекундах
+// // setTimeout - встановлює перерву перед виконанням коду у середині таймаут
 
-console.log('Початок'); //синхроний код
+// console.log('Початок'); //синхроний код
 
-const timeout = setTimeout(
-  (a, b, c, d, e, f, g) => {
-    // console.log(a, b, c, d, e, f);
-    console.log('Привіт');
-  },
-  1000
-  // 1,
-  // 2,
-  // 3,
-  // 4,
-  // 5,
-  // 6
-); //асинхронний
+// const timeout = setTimeout(
+//   (a, b, c, d, e, f, g) => {
+//     // console.log(a, b, c, d, e, f);
+//     console.log('Привіт');
+//   },
+//   1000
+//   // 1,
+//   // 2,
+//   // 3,
+//   // 4,
+//   // 5,
+//   // 6
+// ); //асинхронний
 
-console.log(timeout);
-console.log('Кінець'); //синхроний код
+// console.log(timeout);
+// console.log('Кінець'); //синхроний код
 
-//let seconds = 0; // синхроний код
+// //let seconds = 0; // синхроний код
 
-// const renderHtml = setInterval(() => {
-//   seconds += 1;
-//   // document.body.innerHTML = seconds;
-//   if (seconds === 1) {
-//     clearInterval(renderHtml);
-//   }
+// // const renderHtml = setInterval(() => {
+// //   seconds += 1;
+// //   // document.body.innerHTML = seconds;
+// //   if (seconds === 1) {
+// //     clearInterval(renderHtml);
+// //   }
 
-//   console.log(seconds);
-// }, 1000); // асинхроний код
+// //   console.log(seconds);
+// // }, 1000); // асинхроний код
 
-// console.log(renderHtml);
-// синхроний код
+// // console.log(renderHtml);
+// // синхроний код
 
-// querySelector();
-// querySelectorAll();
-// getElementById();
+// // querySelector();
+// // querySelectorAll();
+// // getElementById();
 
-// const input = document.getElementById('input');
+// // const input = document.getElementById('input');
+// // const counter = document.querySelector('.counter');
+
+// // input.addEventListener('input', onInput);
+// // let interval;
+
+// // function onInput(e) {
+// //   let seconds = e.target.value;
+// //   console.log(seconds);
+
+// //   interval = setInterval(() => {
+// //     counter.innerHTML = seconds;
+// //     if (seconds === -1) {
+// //       clearInterval(interval);
+// //     }
+// //   }, 1000);
+// // }
+
+// const board = document.getElementById('board');
+// const button = document.getElementById('startGame');
 // const counter = document.querySelector('.counter');
 
-// input.addEventListener('input', onInput);
-// let interval;
+// button.addEventListener('click', onClick);
+// board.addEventListener('click', clickOnBoard);
 
-// function onInput(e) {
-//   let seconds = e.target.value;
-//   console.log(seconds);
+// let interval;
+// let score = 0;
+// let seconds = 10;
+
+// function onClick(e) {
+//   board.style.display = 'block';
 
 //   interval = setInterval(() => {
+//     seconds--;
 //     counter.innerHTML = seconds;
 //     if (seconds === -1) {
 //       clearInterval(interval);
+//       alert(`Гру завершено ви отримали ${score} очок`);
 //     }
 //   }, 1000);
 // }
 
-const board = document.getElementById('board');
-const button = document.getElementById('startGame');
-const counter = document.querySelector('.counter');
+// function clickOnBoard(e) {
+//   if (!e.target.classList.contains('gameElement')) return;
+//   score += 200;
 
-button.addEventListener('click', onClick);
-board.addEventListener('click', clickOnBoard);
+//   e.target.style.backgroundColor = 'transparent';
+//   setTimeout(() => {
+//     e.target.style.backgroundColor = 'green';
+//   }, 500);
+// }
+
+const input = document.getElementById('input');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const restart = document.getElementById('restart');
+
+console.log(input, start, stop);
+start.addEventListener('click', onStart);
+stop.addEventListener('click', onStop);
+restart.addEventListener('click', onRestart);
 
 let interval;
-let score = 0;
-let seconds = 10;
+let seconds;
 
-function onClick(e) {
-  board.style.display = 'block';
+function onStart() {
+  seconds = Number(input.value);
+  const ms = seconds / 1000;
 
   interval = setInterval(() => {
     seconds--;
-    counter.innerHTML = seconds;
-    if (seconds === -1) {
+    if (seconds === 0) {
       clearInterval(interval);
-      alert(`Гру завершено ви отримали ${score} очок`);
     }
-  }, 1000);
+    console.log(seconds);
+  }, ms);
 }
 
-function clickOnBoard(e) {
-  if (!e.target.classList.contains('gameElement')) return;
-  score += 200;
+function onStop() {
+  clearInterval(interval);
+}
 
-  e.target.style.backgroundColor = 'transparent';
-  setTimeout(() => {
-    e.target.style.backgroundColor = 'green';
-  }, 500);
+function onRestart(params) {
+  const ms = seconds / 1000;
+
+  interval = setInterval(() => {
+    seconds--;
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+    console.log(seconds);
+  }, ms);
 }
