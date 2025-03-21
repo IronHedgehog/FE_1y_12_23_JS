0e59a7a5959637765a7f54e7de1b3b8096d753c7
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-03-21 20:46:20 +0200

    promise

--- src/index.js
+++ src/index.js
@@ -1,79 +1,31 @@
-// const date = new Date('April 15,1970');
-
-// // const newDate = new Date(year, month, day, hours, minutes, s, ms);
-
-// console.log();
-// // console.log(newYear2);
-// console.log(date);
-// // 2030-05-01T10:20:30 - YYYY-MM-DDTHH:MM:SS:MS
-// console.log(Date.parse('2026-01-01T00:00:00')); //(1682925630000) - мс з 1970р
-// console.log(Date.parse('2026-01-01T00:00:00') - Date.now()); // проміжок часу від зараз до  2026-01-01
-
-// const time = new Date('2025-03-01');
-
-// console.log(time.getDate()); // число місяця
-// console.log(time.getDay()); // видає значення по індексу з 0 до 6 (Починає відлік з неділі, субота 6)
-
-// console.log(time.getMonth()); // видає значення по індексу з 0 до 11
-
-// console.log(time.getFullYear()); // рік
-
-// console.log(time.getHours());
-// console.log(time.getMinutes());
-// console.log(time.getSeconds());
-// console.log(time.getMilliseconds());
-
-// console.log(time.getTime());
-
-// console.log(time.toUTCString());
-// console.log(time.getUTCDay());
-// console.log(time.getUTCMonth());
-
-// const newYear = Date.parse('2026-01-01T00:00:00');
-
-// // console.log(newYear.toLocaleString());
-// // BOM
-// // const locale =
-// console.log(
-//   time.toLocaleString('Uk-uk', {
-//     month: 'long',
-//     day: '2-digit',
-//     year: 'numeric',
-//     hour: '2-digit',
-//     minute: '2-digit',
-//   })
-// );
-// dayPeriod: 'short',
-// hour12: true,
-
-//
-
-function timer() {
-  const newYear = new Date(2026, 0, 1, 0, 0, 0, 0);
-  // const nextYear = new Date().getFullYear() + 1;
-
-  const currentTime = new Date();
-
-  const diff = newYear - currentTime;
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-  console.log(days, hours, minutes, seconds);
-}
-
-// timer();
-
-// let date = new Date().toDateString() + ' ' + new Date().toTimeString();
-
-// console.log(date);
-let start = new Date();
-
-for (let i = 0; i < 100000; i++) {
-  console.log(i);
-}
-
-let end = new Date();
-
-console.log(`Робота циклу зайняла ${end - start}мс`);
+let random = Math.floor(Math.random() * 10);
+console.log(random);
+const promise = new Promise((resolve, reject) => {
+  if (random >= 6) {
+    // resolve- все кул, проміс виконано успішно
+    resolve('Проміс виконано з успіхом');
+  } else {
+    reject('Проміс виконано але без успіху');
+  }
+});
+// then - На практиці працює тільки першим параметром тоб то з успіхом
+promise
+  .then(
+    // 1-й параметр then(колбек який відповідає за успішне виконання промісу)
+    valueResolve => {
+      console.log(valueResolve);
+      console.log('Відпрацював then');
+    }
+    // // 2-й параметр then(колбек який відповідає за не успішне виконання промісу)
+    // valueReject => {
+    //   throw new Error(valueReject);
+    // }
+  )
+  .catch(error => {
+    console.log(error);
+    console.log('Відпрацював catch');
+  })
+  .finally(() => {
+    // Метод finally виконується в будь-якому разі не залежно від результату
+    console.log('Виконання промісу завершено');
+  });
