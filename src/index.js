52e166d71a8434cc189bf2740d17f43fe39ab422
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-03-26 21:24:03 +0200

    promiseMethods

--- src/index.js
+++ src/index.js
@@ -1,99 +1,18 @@
-// let random = Math.floor(Math.random() * 10);
-// console.log(random);
-// const promise = new Promise((resolve, reject) => {
-//   if (random >= 6) {
-//     // resolve- все кул, проміс виконано успішно
-//     resolve('Проміс виконано з успіхом');
-//   } else {
-//     reject('Проміс виконано але без успіху');
-//   }
-// });
-// // then - На практиці працює тільки першим параметром тоб то з успіхом
-// promise
-//   .then(
-//     // 1-й параметр then(колбек який відповідає за успішне виконання промісу)
-//     valueResolve => {
-//       console.log(valueResolve);
-//       console.log('Відпрацював then');
-//     }
-//     // // 2-й параметр then(колбек який відповідає за не успішне виконання промісу)
-//     // valueReject => {
-//     //   throw new Error(valueReject);
-//     // }
-//   )
-//   .catch(error => {
-//     console.log(error);
-//     console.log('Відпрацював catch');
-//   })
-//   .finally(() => {
-//     // Метод finally виконується в будь-якому разі не залежно від результату
-//     console.log('Виконання промісу завершено');
-//   });
+const makePromise = (text, delay) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(text);
+    }, delay);
+  });
+};
 
-// const promise1 = new Promise((resolve, reject) => {
-//   if (random >= 6) {
-//     // resolve- все кул, проміс виконано успішно
-//     resolve('Успіх');
-//   } else {
-//     reject('Не успіх');
-//   }
-// });
+const promise1 = makePromise('promise1', 10000);
+const promise2 = makePromise('promise2', 2000);
+const promise3 = makePromise('promise3', 1000);
 
-// console.log(promise1);
+// Promise.all - повертає проміс
+Promise.all([promise1, promise2, promise3]).then(value => {
+  console.log(value);
+});
 
-// promise1
-//   .then(value => {
-//     console.log(value);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-//   .finally(() => {
-//     console.log('Finish');
-//   });
-
-// Промісифікація функції
-
-// const fetchPosts = (user, onSuccess, onError) => {
-//   setTimeout(() => {
-//     const isOk = false;
-
-//     if (isOk) {
-//       onSuccess('Cool');
-//     } else {
-//       onError('not cool');
-//     }
-//   }, 1000);
-// };
-
-// const onSuccess = user => {
-//   console.log(user);
-// };
-
-// const onError = user => {
-//   console.log(user);
-// };
-
-// console.log(fetchPosts({ name: 'Artem' }, onSuccess, onError));
-
-// const fetchPosts = user => {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       const isOk = false;
-
-//       if (isOk) {
-//         res('Cool');
-//       } else {
-//         rej('not cool');
-//       }
-//     }, 1000);
-//   });
-// };
-
-// fetchPosts({ name: 'Artem' })
-//   .then(value => {
-//     console.log(value);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+Promise.race([promise1, promise2, promise3]).then(value => console.log(value));
