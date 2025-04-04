69f9e353d4c39356c3000e1ebf8303dd00f4ac38
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-04 20:46:28 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -8,14 +8,38 @@ const post = {
   text: 'text',
 };
 
-const data = fetch('https://jsonplaceholder.typicode.com/users', {
-  method: 'POST',
-  body: JSON.stringify(post),
-  // headers: {
-  //   'content-type': 'application/json',
-  // },
-});
-data
+// const data = fetch('https://jsonplaceholder.typicode.com/users', {
+//   method: 'POST',
+//   body: JSON.stringify(post),
+//   // headers: {
+//   //   'content-type': 'application/json',
+//   // },
+// });
+// data
+//   .then(value => {
+//     // Відповідь від сервера шо все круто
+//     // Форматування даних
+//     console.log(value);
+
+//     return value.json();
+//   })
+//   .then(value => {
+//     // ОТримуємо та працюємо з даними
+//     console.log(value);
+//   })
+//   .catch(error => {
+//     // Обробка помилки
+//     console.log(error);
+//   });
+
+// https://newsapi.org/v2/ - шлях до бекенду
+// /top-headlines - ендпоінт
+// ?country=us&apiKey=2667a6ad789c4db2a2e882b6e2781cbb
+const news = fetch(
+  'https://newsapi.org/v2/top-headlines?q=Tesla&apiKey=2667a6ad789c4db2a2e882b6e2781cbb'
+);
+
+news
   .then(value => {
     // Відповідь від сервера шо все круто
     // Форматування даних
