34961807d9e33f9f57b3f7c2e4eee9f3d5867802
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-16 21:37:58 +0300

    svg

--- src/index.js
+++ src/index.js
@@ -1,70 +1,39 @@
-import axios from 'axios';
-import debounce from 'debounce';
-// async - ця функція буде асинхронною
-const getImgs = async valueFromSearchInput => {
-  // const img = fetch(
-  //   'https://pixabay.com/api/?key=37133631-8f51e9c6f744fc90bf0fea549&q=yellow+flowers&image_type=photo'
-  // );
-  // console.log(img);
-  // img
-  //   .then(value => {
-  //     return value.json();
-  //   })
-  //   .then(date => {
-  //     console.log(date);
-  //   });
-  // async await
-  // try catch
-  try {
-    const { data } = await axios.get(
-      `https://pixabay.com/api/?key=37133631-8f51e9c6f744fc90bf0fea549&q=${valueFromSearchInput}&image_type=photo`
-    );
+const newsList = document.getElementById('newsList');
+const loadMoreButton = document.getElementById('load');
 
-    makeHtml(data.hits);
-  } catch (error) {
-    console.log(error);
-  }
+const fetchNews = async (sourse = 'techcrunch') => {
+  try {
+    console.log(sourse);
+    const newsData = await fetch(
+      `https://newsapi.org/v2/top-headlines?sources=${sourse}&apiKey=2667a6ad789c4db2a2e882b6e2781cbb`
+    );
+    const { articles } = await newsData.json();
+
+    makeHtml(articles);
+  } catch (error) {}
 };
 
-const searchInput = document.getElementById('query');
-const imgsList = document.querySelector('.imgs-list');
+fetchNews();
+// document.addEventListener('DOMContentLoaded', fetchNews);
 
-searchInput.addEventListener('input', debounce(searchPhotos, 250));
-
-function searchPhotos(e) {
-  const value = searchInput.value;
-  getImgs(value);
-}
-
-function makeHtml(imgs) {
-  const markup = imgs
-    .map(img => {
-      return `<li class="imgs-item">
-    <img class="img" src="${img.webformatURL}"/>
+function makeHtml(news) {
+  const markup = news
+    .map(news1 => {
+      return `<li class="news-item">
+    <h1>${news1.title}</h1>
+    <p>${news1.author}</p>
+    <p>${news1.content}</p>
     </li>`;
     })
     .join();
-  imgsList.innerHTML = markup;
-  // imgsList.insertAdjacentHTML('beforeend', markup);
+
+  // newsList.innerHTML = markup;
+  newsList.insertAdjacentHTML('beforeend', markup);
 }
 
-{
-  collections: 545;
-  comments: 61;
-  downloads: 56825;
-  id: 5339659;
-  imageHeight: 5821;
-  imageSize: 3762562;
-  imageWidth: 3873;
-  likes: 377;
-  previewHeight: 150;
-  previewWidth: 100;
-  tags: 'church window, monastery, window, stained glass, middle ages, stained glass window, church window, window, window, window, window, window, stained glass';
-  type: 'photo';
-  user: 'Paul_Henri';
-  user_id: 6735189;
-  views: 78873;
-  webformatHeight: 640;
-  webformatURL: 'https://pixabay.com/get/g36fcf0a526fe4f25957a1a53c50cd751ce0a0815d4ca169b43c0c8593591a7a52e520b0469e136da1a4d33cf0e7e41fd02044689f4e236df9f00e0c0064e43cd_640.jpg';
-  webformatWidth: 426;
-}
+const pagination = async params => {
+  const sources = ['aftenposten', 'ars-technica', 'bbc-news', 'techcrunch'];
+  const randomSource = Math.floor(Math.random() * sources.length);
+  await fetchNews(sources[randomSource]);
+};
+loadMoreButton.addEventListener('click', pagination);
